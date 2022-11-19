9370ac48d1e876e665795815545fba56d87a4ba8
Yeshwant Thota <thotayeshwant@gmail.com>
2022-11-19 23:38:14 +0530

    first commit

---
+++ pages/blog.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
+const Blog = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div>
+          <Link href={"/blogpost/learn-javascript"}>
+            <h3 className={styles.blogItemh3}>
+              How to learn JavaScript in 2022?
+            </h3>
+          </Link>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h3>How to learn javascript int 2022</h3>
+          <p>Javascript is the language used to design logic for the web.</p>
+        </div>
+        <div className="blogItem">
+          <h3>How to learn javascript int 2022</h3>
+          <p>Javascript is the language used to design logic for the web.</p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Blog;
